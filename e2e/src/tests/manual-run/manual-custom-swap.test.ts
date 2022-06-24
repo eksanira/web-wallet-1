73ea13fdee18ee7615ae84880089c8a379bd7671
Ihor Karmanov <gariq.lg@gmail.com>
2022-06-24 13:47:31 +0300

    modify test data

--- e2e/src/tests/manual-run/manual-custom-swap.test.ts
+++ e2e/src/tests/manual-run/manual-custom-swap.test.ts
@@ -4,7 +4,8 @@ import { log } from '../../tools/logger';
 
 test.describe('Swap', () => {
   const txHashes: string[] = [];
-  const customAddress = '';
+  const customAddress = '0x9213191373dc805fabdf9975f47f234083eef597';
+  let amount = '0.0001';
   const swapPairs: Currency[][] = [
     ['token-vlx_evm', 'token-vlx_erc20'],
     ['token-vlx_evm', 'token-bsc_vlx'],
@@ -23,7 +24,7 @@ test.describe('Swap', () => {
   ]
 
   test.beforeEach(async ({ auth, wallets }) => {
-    await auth.goto();
+    await auth.goto({environment: 'prod'});
     await auth.loginByRestoringSeed(data.wallets.manualSwap.seed);
     await wallets.waitForWalletsDataLoaded();
   });
@@ -34,8 +35,10 @@ test.describe('Swap', () => {
 
   for (let swapPair of swapPairs) {
     const [fromToken, toToken] = swapPair;
+    if (swapPair == ['token-bsc_vlx', 'token-vlx_evm']) amount = '0.0002';
+    if (swapPair == ['token-vlx_huobi', 'token-vlx_evm']) amount = '0.0004';
     test(`${fromToken} > ${toToken}`, async ({ wallets }) => {
-      await wallets.swapTokens(fromToken, toToken, '0.0001', { customAddress: customAddress });
+      await wallets.swapTokens(fromToken, toToken, amount, { customAddress: customAddress });
       const txHash = await wallets.getTxHashFromTxlink();
       txHashes.push(txHash);
     });
