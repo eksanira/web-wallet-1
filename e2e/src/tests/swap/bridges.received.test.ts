1f850340fbd89218a354b6c15e3c33e19d43acc6
Ihor Karmanov <gariq.lg@gmail.com>
2022-06-25 03:46:41 +0300

    add bridge tests

---
+++ e2e/src/tests/swap/bridges.received.test.ts
@@ -0,0 +1,39 @@
+import { test } from '../../common-test-exports';
+import { Currency } from '../../screens';
+import { log } from '../../tools/logger';
+
+test.describe('Swap', () => {
+  const today = new Date().toLocaleDateString("nu").replace(/\//g, '');
+
+  test.beforeAll(async ({ auth }) => {
+    await auth.goto({environment: 'prod'});
+    await auth.loginByRestoringSeed(today);
+  });
+
+  test('Check balances', async ({ wallets }) => {
+    const tokens : Currency[] = [
+      'token-bsc_vlx', 'token-vlx_huobi', 'token-bnb', 'token-busd', 
+      'token-eth', 'token-huobi', 'token-usdc', 'token-usdt_erc20', 
+      'token-vlx_busd', 'token-vlx_erc20', 'token-vlx_eth', 'token-vlx_evm',
+      'token-vlx_usdc', 'token-vlx_usdt'
+    ]
+
+    for(let token of tokens){
+      await wallets.addToken(token);
+    };
+
+    const balances = await wallets.getWalletsBalances();
+
+    for (const [key, value] of Object.entries(balances)) {
+      if (key === 'token-vlx_evm' && Number(value) < 0.000007){
+          throw new Error (`Balance of ${key} is ${value}, 0.000007 expected`)
+      }
+
+      if (key !== 'token-vlx_evm' && Number(value) === 0){
+        throw new Error (`Balance of ${key} is zero`)
+      }
+    }
+
+    log.debug(balances);
+  });
+});
---
+++ e2e/src/tests/swap/bridges.send.test.ts
@@ -0,0 +1,54 @@
+import { data, test } from '../../common-test-exports';
+import { Currency } from '../../screens';
+import { log } from '../../tools/logger';
+
+test.describe('Swap', () => {
+  const today = new Date().toLocaleDateString("nu").replace(/\//g, '');
+  const txHashes: string[] = [];
+  let receiverAddress = '';
+  let amount = '0.000001';
+  const swapPairs: Currency[][] = [
+    ['token-vlx_evm', 'token-vlx_erc20'],
+    ['token-vlx_evm', 'token-bsc_vlx'],
+    ['token-vlx_evm', 'token-vlx_huobi'],
+    ['token-vlx_usdc', 'token-usdc'],
+    ['token-vlx_usdt', 'token-usdt_erc20'],
+    ['token-vlx_eth', 'token-eth'],
+    ['token-vlx_busd', 'token-busd'],
+    ['token-usdc', 'token-vlx_usdc'],
+    ['token-eth', 'token-vlx_eth'],
+    ['token-vlx_erc20', 'token-vlx_evm'],
+    ['token-usdt_erc20', 'token-vlx_usdt'],
+    ['token-bsc_vlx', 'token-vlx_evm'],
+    ['token-busd', 'token-vlx_busd'],
+    ['token-vlx_huobi', 'token-vlx_evm']
+  ]
+
+  test.beforeAll(async ({ auth, wallets }) => {
+    await auth.goto({environment: 'prod'});
+    await auth.loginByRestoringSeed(today);
+    await wallets.selectWallet('token-vlx_evm');
+    receiverAddress = await wallets.getWalletAddress();    
+  });
+
+  test.beforeEach(async ({ auth, wallets }) => {
+    await auth.goto({environment: 'prod'});
+    await auth.loginByRestoringSeed(data.wallets.manualSwap.seed);
+    await wallets.waitForWalletsDataLoaded();
+  });
+
+  test.afterAll(async () => {
+    log.info(`List of transactions:\n${txHashes}`);
+  });
+
+  for (let swapPair of swapPairs) {
+    const [fromToken, toToken] = swapPair;
+    if (swapPair == ['token-bsc_vlx', 'token-vlx_evm']) amount = '0.000002';
+    if (swapPair == ['token-vlx_huobi', 'token-vlx_evm']) amount = '0.000004';
+    test(`${fromToken} > ${toToken}`, async ({ wallets }) => {
+      await wallets.swapTokens(fromToken, toToken, amount, { customAddress: receiverAddress });
+      const txHash = await wallets.getTxHashFromTxlink();
+      txHashes.push(txHash);
+    });
+  }
+});
